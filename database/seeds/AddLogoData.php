7e46ae1ec2b9e51c7fbb600657d2e24fd680495e
Mainul Islam <mainulkhan94@gmail.com>
2017-03-20 21:06:08 +0600

    Added Logo Seed

---
+++ database/seeds/AddLogoData.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class AddLogoData extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('settings')->insert([
+            'name'=>'logo1',
+            'options'=>'{"file_logo":"settings/ca-logo.png"}'
+        ]);
+    }
+}
